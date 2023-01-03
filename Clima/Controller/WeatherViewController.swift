4d072f93ec70ce6d2ac43ee9984a905f468f6626
Lucien George <lucien.george@shopify.com>
2023-01-03 18:52:47 +0200

    clima app almost done

--- Clima/Controller/WeatherViewController.swift
+++ Clima/Controller/WeatherViewController.swift
@@ -9,16 +9,65 @@
 import UIKit
 
 class WeatherViewController: UIViewController {
-
+//MARK: - Properties
     @IBOutlet weak var conditionImageView: UIImageView!
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var cityLabel: UILabel!
-    
+    @IBOutlet weak var searchTextField: UITextField!
+
+    var weatherService = WeatherService()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
 
+        searchTextField.delegate = self
+        weatherService.delegate = self
+    }
 
 }
 
+//MARK: - UITextFieldDelegate
+extension WeatherViewController: UITextFieldDelegate {
+    @IBAction func searchPressed(_ sender: UIButton) {
+        searchTextField.endEditing(true)
+    }
+
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        searchTextField.endEditing(true)
+        return true
+    }
+
+    //    could be used for validations
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if textField.text != "" {
+            return true
+        } else {
+            textField.placeholder = "Type something"
+            return false
+        }
+    }
+
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        //        use the searchTextField.text to get the weather for that city
+        if let city = searchTextField.text {
+            weatherService.fetchWeather(for: city)
+        }
+        searchTextField.text = ""
+    }
+}
+
+
+//MARK: - WeatherServiceDelegate
+extension WeatherViewController: WeatherServiceDelegate {
+    func didUpdateWeather(_ weatherService: WeatherService, weather: WeatherModel) {
+        DispatchQueue.main.async {
+            self.conditionImageView.image = UIImage(systemName: weather.conditionName)
+            self.cityLabel.text = weather.cityName
+            self.temperatureLabel.text = weather.temperatureString
+        }
+    }
+
+    func didFailWithError(_ weatherService: WeatherService, error: Error) {
+        print(error)
+    }
+}
---
+++ Clima/WeatherService.swift
@@ -0,0 +1,46 @@
+import Foundation
+
+protocol WeatherServiceDelegate {
+    func didUpdateWeather(_ weatherService: WeatherService, weather: WeatherModel)
+    func didFailWithError(_ weatherService: WeatherService, error: Error)
+}
+
+struct WeatherService {
+    let weatherURL = "https://api.openweathermap.org/data/2.5/weather?appid=31ffd6fb6474f838a2bdc0fc22c45e0d&units=metric"
+    var delegate: WeatherServiceDelegate?
+
+    func fetchWeather(for cityName: String) {
+        let urlString = "\(weatherURL)&q=\(cityName.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed)!)"
+        performRequest(with: urlString)
+    }
+
+    private func performRequest(with urlString: String) {
+        if let url = URL(string: urlString) {
+            let session = URLSession(configuration: .default)
+            let task = session.dataTask(with: url) { (data, response, error) in
+                if error != nil {
+                    delegate?.didFailWithError(self, error: error!)
+                    return
+                }
+
+                if let safeData = data, let weather = parseJSON(safeData) {
+                    delegate?.didUpdateWeather(self, weather: weather)
+                }
+            }
+            task.resume()
+        }
+    }
+
+    private func parseJSON(_ weatherData: Data) -> WeatherModel? {
+        do {
+            let decodedData = try JSONDecoder().decode(WeatherData.self, from: weatherData)
+            let id = decodedData.weather[0].id
+            let temperature = decodedData.main.temp
+            let name = decodedData.name
+            return WeatherModel(conditionId: id, cityName: name, temperature: temperature)
+        } catch {
+            delegate?.didFailWithError(self, error: error)
+            return nil
+        }
+    }
+}
